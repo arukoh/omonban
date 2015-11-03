cf23b0e0a38bbc9cd5b9d203f7a4e1c83aa7ca97
kinumi <kunimi.ikeda@gmail.com>
2015-11-03 13:36:16 +0900

    add "auth" option to proxy difinitions

--- lib/oauth/helpers.rb
+++ lib/oauth/helpers.rb
@@ -23,5 +23,15 @@ module OAuth
     def find_oauth(provider)
       settings.oauth.find{|h| h["omniauth"] == provider}
     end
+
+    def auth_is_required?(host, path)
+      proxy = Settings.proxy.find{|h| h["host"] == host && Regexp.new(h["path"]) =~ path}
+      unless proxy
+        proxy = Settings.proxy.find{|h| h["host"] == nil && Regexp.new(h["path"]) =~ path}.to_h
+      end
+      required = !proxy.has_key?("auth") || proxy["auth"]
+      session[:authorized] = true unless required
+      required
+    end
   end
 end
